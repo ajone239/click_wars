1a0ab05237338e5b3570867ff9b108939c3962ce
Austin Jones <ajones53.aj@gmail.com>
2025-05-17 20:15:35 -0400

    whoops

--- colors.js
+++ colors.js
@@ -1,6 +1,10 @@
 class Theme {
-    static background = "#00021b"
-    static primary = "#e51376"
-    static back_highlight = "#3c4c79"
-    static accent1 = "#02d3b0"
+  static background = "#00021b"
+  static primary = "#e51376"
+  static back_highlight = "#3c4c79"
+  static back_highlight2 = "#18214c"
+  static accent1 = "#02d3b0"
+  static accent2 = "#3f887c"
+  static accent3 = "#02d3b0"
+  static accent4 = "#3f887c"
 }
